00506c575539c1977e4746a76aef5397774aeecf
alish9877 <alshaikhali302@gmail.com>
2024-11-08 12:50:41 +0300

    step2

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/*-------------------------------- Constants --------------------------------*/
+const squareEls = [0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8]
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+let board 
+let turn 
+let winner 
+let tie 
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+const messageEl = document.querySelector('#message') 
+console.log(messageEl)
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
